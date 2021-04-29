7898948097f502a5344b5c9192de946fa23f8f03
Mariusz Sokołowski <mariuszeagle98@gmail.com>
2021-04-29 08:59:27 +0200

    Add project files.

---
+++ ASP_NET_Exchange/Models/ExchangeRateFromApi.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASP_NET_Exchange
+{
+    /// <summary>
+    /// this class allow us to fetch and save data to this fields
+    /// ten format jest znany z gory
+    /// </summary>
+    class ExchangeRateFromApi
+    {
+        public string Disclaimer { set; get; }
+        public string License { set; get; }
+        public int timeStamp { set; get; }
+
+        public string Baza { set; get; }
+        public  Dictionary<string,decimal> Rates { set; get; }
+
+    }
+}
